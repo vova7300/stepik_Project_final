c737493c253cb16bab9f6ca7c4fdae968097733f
vova73 <84384370+vova7300@users.noreply.github.com>
2022-02-02 13:10:29 +0300

    Ups

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
-from .pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
-from .pages.login_page import LoginPage
+from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 
 import time
 import pytest
